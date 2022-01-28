b6cef2276056cb6b303e6732ee6af555ec759851
Lautaro Recio <reciolauti@gmail.com>
2022-01-28 00:52:16 -0300

    Componentes 2

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './ItemCount.css'
+
+export default function ItemCount({stock, initial, onAdd}){
+    const [SumarRestar, setSumaResta] = useState(initial)
+
+    function sumar(){
+        setSumaResta(SumarRestar+1)
+        if (SumarRestar === stock){
+            /* Si al restar productos el valor es igual a 1 el valor se iguala a la constante que tiene valor constante*/
+            setSumaResta(stock)
+            alert("No hay mas stock")
+        }
+    }
+    
+    function restar(){
+        setSumaResta(SumarRestar-1)
+        if (SumarRestar === 1){
+            /* Si al restar productos el valor es igual a 1 el valor se iguala a la constante que tiene valor constante*/
+            setSumaResta(SumarRestar)
+            alert("No se pueden restar mas items")
+
+        }
+        
+    }
+
+    return(
+        <div>
+            <div className='contador'>
+                <button onClick={restar}>-</button>
+                <p>{SumarRestar}</p>
+                <button onClick={sumar}>+</button>
+
+            </div>
+            <button onClick={onAdd}>Comprar</button>
+        </div>
+    );
+}            
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './ItemListContainer.css'
-
+import '../ItemCount/ItemCount'
+import ItemCount from '../ItemCount/ItemCount'
 
 function ItemListContainer({imgSrc,prod,greeting,consola}){
+    
+    function addToCart(){
+    
+        console.log("se agrego un producto al carrito")
+
+    }
     return(
         <div className='col-md-3 cards '>
             <img className='logo' src={consola} alt={consola} />
             <img className='imgsProds' src={imgSrc} alt={prod} />
             <p>$ {greeting}</p>
             <p>{prod}</p>
-            <button>Comprar</button>
+            <ItemCount initial={1} stock={10} onAdd={addToCart} />
         </div>
     )  
 }
